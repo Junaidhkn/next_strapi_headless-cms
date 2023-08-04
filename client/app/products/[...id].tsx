24df0c4ae70b2a9fd1c056ca0d31e77a2e0fea61
junaidhkn <junaidhkniazi@gmail.com>
2023-08-05 03:10:57 +0500

    slug

--- client/app/products/[...id].tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const itemDetail = () => {
-	return <div>itemDetail</div>;
-};
-
-export default itemDetail;
---
+++ client/app/products/[...id]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const itemDetailPage = () => {
+	return <div>itemDetailPage</div>;
+};
+
+export default itemDetailPage;
