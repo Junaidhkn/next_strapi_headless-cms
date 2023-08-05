a8f765a59d174f702c02bbef28a694e7ebdeac1c
junaidhkn <junaidhkniazi@gmail.com>
2023-08-06 00:36:40 +0500

    navbar

--- client/components/global/Navbar.tsx
+++ client/components/global/Navbar.tsx
@@ -10,25 +10,35 @@ import { Input } from '@/components/ui/input';
 
 const Navbar = () => {
 	return (
-		<nav className='h-16 w-full bg-[#262828] flex justify-between text-white items-center px-11'>
-			<div className='flex-grow'>
-				<Link href='/'>
-					<h1 className='text-3xl'>Logo/ title</h1>
-				</Link>
-			</div>
-			<div className='flex'>
-				<div>
-					<Link href='/'></Link>
+		<nav className='h-16 w-full bg-[#262828] flex flex-grow basis-auto justify-between text-white items-center'>
+			<div className='flex justify-around items-center'>
+				<div className='flex items-center'>
+					<Link href='/'>
+						<h1 className='text-3xl mr-1'>Ecom Things</h1>
+					</Link>
+					<div className='h-16 w-[0.1px] bg-white'></div>
 				</div>
-				<div>
-					<Link href='/'></Link>
+				<div className='flex justify-around ml-0 h-16 border border-white items-center'>
+					<div className=' h-full w-7'>
+						<Link href='/men'>MEN</Link>
+					</div>
+					<div>
+						<Link href='/women'>WOMEN</Link>
+					</div>
+				</div>
+				<div className='flex text-gray-900 relative'>
+					<Input
+						type='search'
+						className='py-3 px-9 text-lg'
+						placeholder='Search for products'
+					/>
+					<FontAwesomeIcon
+						icon={faMagnifyingGlass}
+						className=' font-extrabold cursor-pointer text-green-600 absolute right-0 top-0 bottom-0 m-auto mr-3'
+					/>
 				</div>
 			</div>
-			<div className='flex flex-grow text-gray-900'>
-				<Input type='search' />
-				<FontAwesomeIcon icon={faMagnifyingGlass} />
-			</div>
-			<div className='flex flex-grow text-2xl'>
+			<div className='flex flex-grow basis-auto text-2xl'>
 				<FontAwesomeIcon icon={faCartShopping} />
 				<Link href='/signin'>
 					<FontAwesomeIcon icon={faUser} />
