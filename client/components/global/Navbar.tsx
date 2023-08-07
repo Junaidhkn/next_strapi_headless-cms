366f669d8d4fb5b52ae3f47a895c95014aab30a2
junaidhkn <junaidhkniazi@gmail.com>
2023-08-08 01:52:00 +0500

    navbar complete

--- client/components/global/Navbar.tsx
+++ client/components/global/Navbar.tsx
@@ -10,37 +10,49 @@ import { Input } from '@/components/ui/input';
 
 const Navbar = () => {
 	return (
-		<nav className='h-16 w-full bg-[#262828] flex flex-grow basis-auto justify-between text-white items-center'>
-			<div className='flex justify-around items-center'>
-				<div className='flex items-center'>
+		<nav className='h-16 w-full bg-[#262828] flex justify-evenly items-center text-white items-center'>
+			<div className='flex basis-[20%]'>
+				<div className='flex items-center mr-2'>
 					<Link href='/'>
-						<h1 className='text-3xl mr-1'>Ecom Things</h1>
+						<h1 className='text-3xl px-3 mr-1'>Ecom</h1>
 					</Link>
 					<div className='h-16 w-[0.1px] bg-white'></div>
 				</div>
-				<div className='flex justify-around ml-0 h-16 border border-white items-center'>
-					<div className=' h-full w-7'>
-						<Link href='/men'>MEN</Link>
+				<div className='flex items-center'>
+					<div>
+						<Link
+							className='px-3 py-5 hover:bg-neutral-600'
+							href='/men'>
+							MEN
+						</Link>
 					</div>
 					<div>
-						<Link href='/women'>WOMEN</Link>
+						<Link
+							className='px-3 py-5 hover:bg-neutral-600'
+							href='/women'>
+							WOMEN
+						</Link>
 					</div>
 				</div>
-				<div className='flex text-gray-900 relative'>
-					<Input
-						type='search'
-						className='py-3 px-9 text-lg'
-						placeholder='Search for products'
-					/>
-					<FontAwesomeIcon
-						icon={faMagnifyingGlass}
-						className=' font-extrabold cursor-pointer text-green-600 absolute right-0 top-0 bottom-0 m-auto mr-3'
-					/>
-				</div>
 			</div>
-			<div className='flex flex-grow basis-auto text-2xl'>
-				<FontAwesomeIcon icon={faCartShopping} />
-				<Link href='/signin'>
+			<div className='flex basis-[40%] text-gray-900 relative'>
+				<Input
+					type='search'
+					className='py-3 px-9 text-lg'
+					placeholder='Search for products'
+				/>
+				<FontAwesomeIcon
+					icon={faMagnifyingGlass}
+					className=' font-extrabold cursor-pointer text-green-600 absolute right-0 top-0 bottom-0 m-auto mr-3'
+				/>
+			</div>
+			<div className='flex items-center justify-end basis-[20%] text-2xl'>
+				<div className='px-3 mx-4 cursor-pointer'>
+					<FontAwesomeIcon icon={faCartShopping} />
+				</div>
+				<Link
+					className='px-3'
+					href='/signin'>
 					<FontAwesomeIcon icon={faUser} />
 				</Link>
 			</div>
