3274c9b8d1215a0e595acb69a15f6a0177eac8f0
junaidhkn <junaidhkniazi@gmail.com>
2023-08-14 11:56:16 +0500

    tooltip

--- client/components/global/Marketing.tsx
+++ client/components/global/Marketing.tsx
@@ -1,22 +1,44 @@
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from '@/components/ui/tooltip';
+
 const Marketing = () => {
 	return (
 		<div className='flex flex-col w-full'>
 			<div className='flex h-16'>
-				<div className='basis-[50%] h-full flex items-center justify-center bg-black text-white text-center'>
-					<p className='text-sm tracking-wide'>
-						<p>UP TO 50% OFF EVERYTHING!*</p>
-						<p>Discounts revealed at checkout</p>
-						<p>
-							With code:
-							<span className='ml-2 uppercase font-extrabold'>BIGDEAL</span>
-						</p>
-					</p>
+				<div className='pointer basis-[50%] h-full flex items-center justify-center bg-black text-white text-center'>
+					<TooltipProvider>
+						<Tooltip>
+							<TooltipTrigger>
+								<p className='text-sm tracking-wide'>
+									<p>UP TO 50% OFF EVERYTHING!*</p>
+									<p>Discounts revealed at checkout</p>
+									<p>
+										With code:
+										<span className='ml-2 uppercase font-extrabold'>
+											BIGDEAL
+										</span>
+									</p>
+								</p>
+							</TooltipTrigger>
+							<TooltipContent side='bottom'>
+								<p className='text-sm max-w-xs'>
+									*Enter code BIGDEAL at checkout to receive discount.
+									Percentage discounts will vary depending upon the product
+									purchased. Ends 8am UTC+1 on 14 August 2023.
+								</p>
+							</TooltipContent>
+						</Tooltip>
+					</TooltipProvider>
 				</div>
 				<div className='delivery flex items-center justify-center  basis-[50%] h-full bg-pink-600 text-center'>
 					Free Worldwide delivery
 				</div>
 			</div>
-			<div className='flex flex-col text-center'></div>
+			<div className='flex flex-col text-center'>max-w-xs</div>
 		</div>
 	);
 };
