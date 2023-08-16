c22e597cab50bce65a57b0b25afcda206180a893
junaidhkn <junaidhkniazi@gmail.com>
2023-08-16 07:43:39 +0500

    pages

--- client/app/men/page.tsx
+++ client/app/men/page.tsx
@@ -1,7 +1,268 @@
-import React from 'react';
+import Image from 'next/image';
 
-const ProductsPage = () => {
-	return <div>ProductsPage</div>;
+import CoverImage from '@/assets/florencia-simonini-PDZAMYvduVk-unsplash.jpeg';
+import Link from 'next/link';
+
+const MenProductsPage = () => {
+	return (
+		<main className='flex justify-center'>
+			<div className='p-11 grid gap-8 grid-cols-4'>
+				<Link href={'men/asdasd'}>
+					<div className='products-card'>
+						<div className='product-image'>
+							<span className='discount-tag'>50% off</span>
+							<Image
+								src={CoverImage}
+								alt='Cover Image'
+								width={800}
+								height={800}
+							/>
+							<button className='card-btn'>add to cart</button>
+						</div>
+						<div className='product-info'>
+							<h2 className='product-brand'>brand</h2>
+							<p className='product-short-description'>
+								a short line about the cloth..
+							</p>
+							<span className='price'>$20</span>
+						</div>
+					</div>
+				</Link>
+				<Link href={'men/asdasd'}>
+					<div className='products-card'>
+						<div className='product-image'>
+							<span className='discount-tag'>50% off</span>
+							<Image
+								src={CoverImage}
+								alt='Cover Image'
+								width={800}
+								height={800}
+							/>
+							<button className='card-btn'>add to cart</button>
+						</div>
+						<div className='product-info'>
+							<h2 className='product-brand'>brand</h2>
+							<p className='product-short-description'>
+								a short line about the cloth..
+							</p>
+							<span className='price'>$20</span>
+						</div>
+					</div>
+				</Link>
+				<Link href={'men/asdasd'}>
+					<div className='products-card'>
+						<div className='product-image'>
+							<span className='discount-tag'>50% off</span>
+							<Image
+								src={CoverImage}
+								alt='Cover Image'
+								width={800}
+								height={800}
+							/>
+							<button className='card-btn'>add to cart</button>
+						</div>
+						<div className='product-info'>
+							<h2 className='product-brand'>brand</h2>
+							<p className='product-short-description'>
+								a short line about the cloth..
+							</p>
+							<span className='price'>$20</span>
+						</div>
+					</div>
+				</Link>
+				<Link href={'men/asdasd'}>
+					<div className='products-card'>
+						<div className='product-image'>
+							<span className='discount-tag'>50% off</span>
+							<Image
+								src={CoverImage}
+								alt='Cover Image'
+								width={800}
+								height={800}
+							/>
+							<button className='card-btn'>add to cart</button>
+						</div>
+						<div className='product-info'>
+							<h2 className='product-brand'>brand</h2>
+							<p className='product-short-description'>
+								a short line about the cloth..
+							</p>
+							<span className='price'>$20</span>
+						</div>
+					</div>
+				</Link>
+				<Link href={'men/asdasd'}>
+					<div className='products-card'>
+						<div className='product-image'>
+							<span className='discount-tag'>50% off</span>
+							<Image
+								src={CoverImage}
+								alt='Cover Image'
+								width={800}
+								height={800}
+							/>
+							<button className='card-btn'>add to cart</button>
+						</div>
+						<div className='product-info'>
+							<h2 className='product-brand'>brand</h2>
+							<p className='product-short-description'>
+								a short line about the cloth..
+							</p>
+							<span className='price'>$20</span>
+						</div>
+					</div>
+				</Link>
+				<Link href={'men/asdasd'}>
+					<div className='products-card'>
+						<div className='product-image'>
+							<span className='discount-tag'>50% off</span>
+							<Image
+								src={CoverImage}
+								alt='Cover Image'
+								width={800}
+								height={800}
+							/>
+							<button className='card-btn'>add to cart</button>
+						</div>
+						<div className='product-info'>
+							<h2 className='product-brand'>brand</h2>
+							<p className='product-short-description'>
+								a short line about the cloth..
+							</p>
+							<span className='price'>$20</span>
+						</div>
+					</div>
+				</Link>
+				<Link href={'men/asdasd'}>
+					<div className='products-card'>
+						<div className='product-image'>
+							<span className='discount-tag'>50% off</span>
+							<Image
+								src={CoverImage}
+								alt='Cover Image'
+								width={800}
+								height={800}
+							/>
+							<button className='card-btn'>add to cart</button>
+						</div>
+						<div className='product-info'>
+							<h2 className='product-brand'>brand</h2>
+							<p className='product-short-description'>
+								a short line about the cloth..
+							</p>
+							<span className='price'>$20</span>
+						</div>
+					</div>
+				</Link>
+				<Link href={'men/asdasd'}>
+					<div className='products-card'>
+						<div className='product-image'>
+							<span className='discount-tag'>50% off</span>
+							<Image
+								src={CoverImage}
+								alt='Cover Image'
+								width={800}
+								height={800}
+							/>
+							<button className='card-btn'>add to cart</button>
+						</div>
+						<div className='product-info'>
+							<h2 className='product-brand'>brand</h2>
+							<p className='product-short-description'>
+								a short line about the cloth..
+							</p>
+							<span className='price'>$20</span>
+						</div>
+					</div>
+				</Link>
+				<Link href={'men/asdasd'}>
+					<div className='products-card'>
+						<div className='product-image'>
+							<span className='discount-tag'>50% off</span>
+							<Image
+								src={CoverImage}
+								alt='Cover Image'
+								width={800}
+								height={800}
+							/>
+							<button className='card-btn'>add to cart</button>
+						</div>
+						<div className='product-info'>
+							<h2 className='product-brand'>brand</h2>
+							<p className='product-short-description'>
+								a short line about the cloth..
+							</p>
+							<span className='price'>$20</span>
+						</div>
+					</div>
+				</Link>
+				<Link href={'men/asdasd'}>
+					<div className='products-card'>
+						<div className='product-image'>
+							<span className='discount-tag'>50% off</span>
+							<Image
+								src={CoverImage}
+								alt='Cover Image'
+								width={800}
+								height={800}
+							/>
+							<button className='card-btn'>add to cart</button>
+						</div>
+						<div className='product-info'>
+							<h2 className='product-brand'>brand</h2>
+							<p className='product-short-description'>
+								a short line about the cloth..
+							</p>
+							<span className='price'>$20</span>
+						</div>
+					</div>
+				</Link>
+				<Link href={'men/asdasd'}>
+					<div className='products-card'>
+						<div className='product-image'>
+							<span className='discount-tag'>50% off</span>
+							<Image
+								src={CoverImage}
+								alt='Cover Image'
+								width={800}
+								height={800}
+							/>
+							<button className='card-btn'>add to cart</button>
+						</div>
+						<div className='product-info'>
+							<h2 className='product-brand'>brand</h2>
+							<p className='product-short-description'>
+								a short line about the cloth..
+							</p>
+							<span className='price'>$20</span>
+						</div>
+					</div>
+				</Link>
+				<Link href={'men/asdasd'}>
+					<div className='products-card'>
+						<div className='product-image'>
+							<span className='discount-tag'>50% off</span>
+							<Image
+								src={CoverImage}
+								alt='Cover Image'
+								width={800}
+								height={800}
+							/>
+							<button className='card-btn'>add to cart</button>
+						</div>
+						<div className='product-info'>
+							<h2 className='product-brand'>brand</h2>
+							<p className='product-short-description'>
+								a short line about the cloth..
+							</p>
+							<span className='price'>$20</span>
+						</div>
+						L
+					</div>
+				</Link>
+			</div>
+		</main>
+	);
 };
 
-export default ProductsPage;
+export default MenProductsPage;
