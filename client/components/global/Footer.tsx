17742dd05f5fb57e7bcb6312534ff114e7374c14
junaidhkn <junaidhkniazi@gmail.com>
2023-08-16 08:11:34 +0500

    footer

--- client/components/global/Footer.tsx
+++ client/components/global/Footer.tsx
@@ -3,41 +3,48 @@ import Link from 'next/link';
 const Footer = () => {
 	return (
 		<>
-			<div className='flex justify-center gap-8 items-center h-14 bg-slate-600'>
+			<div className='my-20 flex justify-center gap-8 items-center h-14 bg-slate-50'>
 				<div className='bg-white'>Social Icons</div>
 				<span className='bg-slate-800 h-8 w-[0.5px]'></span>
 				<div className='bg-rose-800'>card icon</div>
 			</div>
 			<div className='flex flex-col'>
-				<div className='flex bg-slate-400 text-gray-700 w-full h-16'>
-					<div className='flex basis-[30%] items-center'>
-						<h1 className='text-3xl px-3 mr-1 h-full bg-slate-900 flex items-center'>
-							Ecom
-						</h1>
+				<div className='flex items-center bg-slate-300 text-gray-700 w-full h-16'>
+					<div className='flex basis-[10%] items-center'>
+						<Link href='/'>
+							<h1 className='text-3xl px-3 mr-1 h-16 bg-slate-900 flex items-center'>
+								Ecom
+							</h1>
+						</Link>
 					</div>
-					<div className='basis-[70%]'>
-						<ul className='flex'>
-							<Link href='/men'>
-								<li className=''>Men</li>
-							</Link>
-							<Link href='/women'>
-								<li className=''>Women</li>
-							</Link>
-							<Link href='/best-sellers'>
-								<li className=''>Best Sellers</li>
-							</Link>
-							<Link href='/new-arrivals'>
-								<li className=''>New Arrivals</li>
-							</Link>
-							<Link href='/top-rated'>
-								<li className=''>Top Rated</li>
-							</Link>
+					<div className='flex justify-between basis-[90%]'>
+						<ul className='flex gap-36 items-center'>
+							<div className='flex justify-start gap-4'>
+								<Link href='/men'>
+									<li className='px-5'>Men</li>
+								</Link>
+								<Link href='/women'>
+									<li className='px-5'>Women</li>
+								</Link>
+							</div>
+							<span className='bg-slate-800 h-8 w-[0.5px]'></span>
+							<div className='flex gap-6'>
+								<Link href='/best-sellers'>
+									<li className='px-5'>Best Sellers</li>
+								</Link>
+								<Link href='/top-rated'>
+									<li className='px-5'>Top Rated</li>
+								</Link>
+								<Link href='/best-sellers'>
+									<li className='px-5'>New Arrivals</li>
+								</Link>
+							</div>
 						</ul>
 					</div>
 				</div>
-				<footer className=' flex w-full h-6 bg-black/50'>
-					<p className=''>copyrights</p>
-					<p className=''></p>
+				<footer className='flex justify-evenly items-center w-full h-6 bg-orange-200'>
+					<p className=''>&copy; 2023-26 ECOM. All rights reserved</p>
+					<p className=''>Made with ❤️ by Junaid Hassan Khan</p>
 				</footer>
 			</div>
 		</>
