bc3a7b4b833dd364927e4e836773ce613021d29f
junaidhkn <junaidhkniazi@gmail.com>
2023-08-18 17:56:04 +0500

    custom scrollbar

--- client/components/global/ShoppingCart.tsx
+++ client/components/global/ShoppingCart.tsx
@@ -9,8 +9,8 @@ import { RiDeleteBinFill } from 'react-icons/ri';
 
 const ShoppingCart = () => {
 	return (
-		<main className='fixed right-0 top-16 -bottom-9 w-[470px] h-full bg-slate-800/75 overflow-y-scroll '>
-			<div className='p-6 w-full flex flex-col bg-slate-300 '>
+		<main className='fixed right-0 top-16 bottom-0 w-[470px] h-full'>
+			<div className='p-6 w-full flex flex-col bg-slate-300 max-h-[550px] overflow-y-scroll'>
 				<button className='absolute w-8 p-2 text-lg top-4 left-5'>
 					<ImCross />
 				</button>
@@ -21,94 +21,104 @@ const ShoppingCart = () => {
 					<p className='font-light italic'>Total Items: 4</p>
 					<span className=' w-64 h-[0.3px] bg-slate-400'></span>
 				</div>
-				<div className='flex flex-col gap-7'>
-					<div className='flex justify-evenly gap-6 border bottom-1 p-2'>
-						<div className='flex flex-col'>
-							<h3 className='mb-4'>Product Name</h3>
-							<p className='mb-4'>Price</p>
-							<div className='flex mb-7'>
-								<button className='p-2 bg-slate-700 text-yellow-50'>
-									<AiOutlinePlus />
-								</button>
-								<p className=' p-3'>Quantity</p>
-								<button className='p-2 bg-slate-700 text-yellow-50'>
-									<AiOutlineMinus />
-								</button>
-							</div>
-							<button className='bg-red-300 text-red-950 rounded-xl px-4 py-2 flex items-center justify-center'>
-								Delete
-								<RiDeleteBinFill />
-							</button>
-						</div>
-						<div className='order-1'>
-							<Image
-								className=' h-52'
-								src={CoverImage}
-								priority={true}
-								width={150}
-								height={150}
-								alt='asdasdasd asdasd'
-							/>
-						</div>
-					</div>
-					<div className='flex justify-evenly gap-6  border bottom-1 p-2'>
-						<div className='flex flex-col'>
-							<h3 className='mb-4'>Product Name</h3>
-							<p className='mb-4'>Price</p>
-							<div className='flex mb-7'>
-								<button className='p-2 bg-slate-700 text-yellow-50'>
-									<AiOutlinePlus />
-								</button>
-								<p className=' p-3'>Quantity</p>
-								<button className='p-2 bg-slate-700 text-yellow-50'>
-									<AiOutlineMinus />
+				<div className='max-h-[380px] overflow-y-scroll'>
+					<div className='flex flex-col gap-7'>
+						<div className='flex justify-evenly gap-6 border bottom-1 p-2'>
+							<div className='flex flex-col'>
+								<h3 className='mb-4'>Product Name</h3>
+								<p className='mb-4'>Price</p>
+								<div className='flex mb-7'>
+									<button className='p-2 bg-slate-700 text-yellow-50'>
+										<AiOutlinePlus />
+									</button>
+									<p className=' p-3'>Quantity</p>
+									<button className='p-2 bg-slate-700 text-yellow-50'>
+										<AiOutlineMinus />
+									</button>
+								</div>
+								<button className='bg-red-300 text-red-950 rounded-xl px-4 py-2 flex items-center justify-evenly'>
+									Delete
+									<RiDeleteBinFill />
 								</button>
 							</div>
-							<button className='bg-red-300 text-red-950 rounded-xl px-4 py-2 flex items-center justify-center'>
-								Delete
-								<RiDeleteBinFill />
-							</button>
+							<div className='order-1'>
+								<Image
+									className=' h-52'
+									src={CoverImage}
+									priority={true}
+									width={150}
+									height={150}
+									alt='asdasdasd asdasd'
+								/>
+							</div>
 						</div>
-						<div className='order-1'>
-							<Image
-								className=' h-52'
-								src={CoverImage}
-								priority={true}
-								width={150}
-								height={150}
-								alt='asdasdasd asdasd'
-							/>
-						</div>
-					</div>
-					<div className='flex justify-evenly gap-6  border bottom-1 p-2'>
-						<div className='flex flex-col'>
-							<h3 className='mb-4'>Product Name</h3>
-							<p className='mb-4'>Price</p>
-							<div className='flex mb-7'>
-								<button className='p-2 bg-slate-700 text-yellow-50'>
-									<AiOutlinePlus />
-								</button>
-								<p className=' p-3'>Quantity</p>
-								<button className='p-2 bg-slate-700 text-yellow-50'>
-									<AiOutlineMinus />
+						<div className='flex justify-evenly gap-6  border bottom-1 p-2'>
+							<div className='flex flex-col'>
+								<h3 className='mb-4'>Product Name</h3>
+								<p className='mb-4'>Price</p>
+								<div className='flex mb-7'>
+									<button className='p-2 bg-slate-700 text-yellow-50'>
+										<AiOutlinePlus />
+									</button>
+									<p className=' p-3'>Quantity</p>
+									<button className='p-2 bg-slate-700 text-yellow-50'>
+										<AiOutlineMinus />
+									</button>
+								</div>
+								<button className='bg-red-300 text-red-950 rounded-xl px-4 py-2 flex items-center justify-center'>
+									Delete
+									<RiDeleteBinFill />
 								</button>
 							</div>
-							<button className='bg-red-300 text-red-950 rounded-xl px-4 py-2 flex items-center justify-center'>
-								Delete
-								<RiDeleteBinFill />
-							</button>
+							<div className='order-1'>
+								<Image
+									className=' h-52'
+									src={CoverImage}
+									priority={true}
+									width={150}
+									height={150}
+									alt='asdasdasd asdasd'
+								/>
+							</div>
 						</div>
-						<div className='order-1'>
-							<Image
-								className=' h-52'
-								src={CoverImage}
-								priority={true}
-								width={150}
-								height={150}
-								alt='asdasdasd asdasd'
-							/>
+						<div className='flex justify-evenly gap-6  border bottom-1 p-2'>
+							<div className='flex flex-col'>
+								<h3 className='mb-4'>Product Name</h3>
+								<p className='mb-4'>Price</p>
+								<div className='flex mb-7'>
+									<button className='p-2 bg-slate-700 text-yellow-50'>
+										<AiOutlinePlus />
+									</button>
+									<p className=' p-3'>Quantity</p>
+									<button className='p-2 bg-slate-700 text-yellow-50'>
+										<AiOutlineMinus />
+									</button>
+								</div>
+								<button className='bg-red-300 text-red-950 rounded-xl px-4 py-2 flex items-center justify-center'>
+									Delete
+									<RiDeleteBinFill />
+								</button>
+							</div>
+							<div className='order-1'>
+								<Image
+									className=' h-52'
+									src={CoverImage}
+									priority={true}
+									width={150}
+									height={150}
+									alt='asdasdasd asdasd'
+								/>
+							</div>
 						</div>
 					</div>
+				</div>
+				<div className='mt-11 flex flex-col items-center justify-center'>
+					<h1 className='font-light italic text-2xl text-center'>
+						Total Price: $500
+					</h1>
+					<button className='mt-8 px-6 py-3 bg-blue-500 text-yellow-100 rounded-lg'>
+						Proceed to Checkout
+					</button>
 				</div>
 			</div>
 		</main>
