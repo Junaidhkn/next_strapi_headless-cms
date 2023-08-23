cabb2320823df09edb8fbe10dc6d4d10cd9dab7b
junaidhkn <junaidhkniazi@gmail.com>
2023-08-23 17:07:01 +0500

    pages products

--- client/app/men/page.tsx
+++ client/app/men/page.tsx
@@ -1,9 +1,16 @@
 import Tabs from '@/components/global/Tabs';
 import Products from '@/components/global/Products';
 
-const products = {};
+const productsMen = async () => {
+	const res = await fetch(
+		'http://localhost:1337/api/items?populate=*&filters[gender]=male',
+	);
+	const products = await res.json();
+	return products;
+};
 
 const MenProductsPage = async () => {
+	const products = await productsMen();
 	return (
 		<main className='flex flex-col justify-center'>
 			<Tabs />
