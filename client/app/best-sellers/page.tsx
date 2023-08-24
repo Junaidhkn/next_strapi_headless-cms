0a2c29ee286ffd81fd001fd6ba41958753cd7ae8
junaidhkn <junaidhkniazi@gmail.com>
2023-08-24 22:29:31 +0500

    fixed

--- client/app/best-sellers/page.tsx
+++ client/app/best-sellers/page.tsx
@@ -2,7 +2,7 @@ import Products from '@/components/global/Products';
 
 const BestSellers = async () => {
 	const res = await fetch(
-		'ttp://localhost:1337/api/items?populate=*&filters[category][$eq]=bestSellers',
+		'http://localhost:1337/api/items?populate=*&filters[category][$eq]=bestSellers',
 	);
 	const products = await res.json();
 	return products;
